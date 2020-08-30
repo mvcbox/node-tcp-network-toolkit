f3faa60749e16a0227619bff2f9c819f2ee3d950
mvcbox <mvcbox.org@gmail.com>
2020-08-30 18:40:42 +0300

    Fix Hydrator types

--- src/Hydrator/Hydrator.ts
+++ src/Hydrator/Hydrator.ts
@@ -4,17 +4,25 @@ import { RawPacket } from '../PacketReader';
 export class Hydrator {
     public protocols: Map<number, typeof Protocol>;
 
-    public constructor(protocols: Protocol[] | {[key: string]: typeof Protocol}) {
+    public constructor(protocols: (typeof Protocol)[] | {[key: string]: typeof Protocol}) {
         this.protocols = new Map<number, typeof Protocol>();
 
-        for (let protocol of <any>protocols) {
-            if ((protocol && protocol.prototype) instanceof Protocol) {
-                if (this.protocols.has(protocol._opcode)) {
-                    throw new Error('Duplicate for "_opcode": ' + protocol._opcode);
-                }
-
-                this.protocols.set(protocol._opcode, protocol);
+        if (Array.isArray(protocols)) {
+            protocols.forEach(item => this._handleProtocolItem(item));
+        } else {
+            for (const key in protocols) {
+                this._handleProtocolItem(protocols[key]);
             }
+        }
+    }
+
+    protected _handleProtocolItem(protocol: typeof Protocol): void {
+        if (protocol?.prototype instanceof Protocol) {
+            if (this.protocols.has(protocol._opcode)) {
+                throw new Error('Duplicate for "_opcode": ' + protocol._opcode);
+            }
+
+            this.protocols.set(protocol._opcode, protocol);
         }
     }
 
@@ -23,7 +31,7 @@ export class Hydrator {
     }
 
     public hydrate(rawPacket: RawPacket): Protocol {
-        const protocol: typeof Protocol | undefined = this.protocols.get(rawPacket.opcode);
+        const protocol = this.protocols.get(rawPacket.opcode);
 
         if (!protocol) {
             throw new Error('Missing protocol for "opcode": ' + rawPacket.opcode);
